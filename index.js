60f63ddabdd4eec0f97f50de965c5711205d9054
DennyBui <dbuidmd2018@gmail.com>
2023-03-07 15:04:02 -0800

    looping code lab complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+const messageNames = [];
+function writeCards(names, value) {
+    let messageNames = []
+    for (let i = 0; i < names.length; i++) {
+        messageNames.push(`Thank you, ${names[i]}, for the wonderful ${value} gift!`);
+    }
+    return messageNames;
+}
+writeCards(["Guadelupe", "Ollie", "Aki"], "Surprise");
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+    return number;
+}
